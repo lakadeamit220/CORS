b0ff76bdb4b1f42aed573be87941c0c5e1d3d308
Amit Lakade <99075733+lakadeamit220@users.noreply.github.com>
2025-06-25 21:16:31 +0530

    Axios installation

--- cors-frontend/src/App.jsx
+++ cors-frontend/src/App.jsx
@@ -1,7 +1,10 @@
+import CORSExamples from "./components/CORSExamples";
+
 function App() {
   return (
     <>
       <h1 className="text-center text-4xl text-green-500 font-bold">CORS</h1>
+      <CORSExamples />
     </>
   );
 }
